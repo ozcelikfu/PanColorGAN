1f8fc9f00c8fa0862e3c06e4d76c63991dce2cf5
Furkan Ozcelik <ozcelikfu@itu.edu.tr>
2020-06-29 18:03:39 +0300

    Network Print

--- networks.py
+++ networks.py
@@ -115,3 +115,10 @@ def define_D(input_nc, ndf, norm='batch', use_sigmoid=False, n_layers=3, gpu_ids
         netD.cuda()
     netD.apply(weights_init)
     return netD
+
+def print_network(net):
+    num_params = 0
+    for param in net.parameters():
+        num_params += param.numel()
+    print(net)
+    print('Total number of parameters: %d' % num_params)
