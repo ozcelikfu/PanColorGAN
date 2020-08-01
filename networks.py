509bb3b62b9a5f6ef518684ab621c33d78ffa3ba
ozcelikfu <ozcelikfu@itu.edu.tr>
2020-08-01 17:27:54 +0300

    Indentation errors are fixed for get_target_tensor function

--- networks.py
+++ networks.py
@@ -145,7 +145,7 @@ class GANLoss(nn.Module):
                 # self.real_label_var = Variable(
                 #    real_tensor, requires_grad=False)
                 self.real_label_var = real_tensor
-                target_tensor = self.real_label_var
+            target_tensor = self.real_label_var
         else:
             create_label = ((self.fake_label_var is None) or
                         (self.fake_label_var.numel() != input.numel()))
@@ -154,7 +154,7 @@ class GANLoss(nn.Module):
                 # self.fake_label_var = Variable(
                 #    fake_tensor, requires_grad=False)
                 self.fake_label_var = fake_tensor
-                target_tensor = self.fake_label_var
+            target_tensor = self.fake_label_var
         return target_tensor
 
     def __call__(self, input, target_is_real):
